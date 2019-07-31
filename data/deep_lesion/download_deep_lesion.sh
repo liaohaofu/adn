1031ed63659e926a1fad0daf5124b31979ab5ed9
Haofu Liao <liaohaofu@gmail.com>
2019-07-31 17:45:56 -0400

    Added a downloading file for DeepLesion

---
+++ data/deep_lesion/download_deep_lesion.sh
@@ -0,0 +1,21 @@
+files="https://nihcc.box.com/shared/static/sp5y2k799v4x1x77f7w1aqp26uyfq7qz.zip
+https://nihcc.box.com/shared/static/l9e1ys5e48qq8s409ua3uv6uwuko0y5c.zip
+https://nihcc.box.com/shared/static/48jotosvbrw0rlke4u88tzadmabcp72r.zip
+https://nihcc.box.com/shared/static/xa3rjr6nzej6yfgzj9z6hf97ljpq1wkm.zip
+https://nihcc.box.com/shared/static/58ix4lxaadjxvjzq4am5ehpzhdvzl7os.zip
+https://nihcc.box.com/shared/static/cfouy1al16n0linxqt504n3macomhdj8.zip
+https://nihcc.box.com/shared/static/z84jjstqfrhhlr7jikwsvcdutl7jnk78.zip
+https://nihcc.box.com/shared/static/6viu9bqirhjjz34xhd1nttcqurez8654.zip
+https://nihcc.box.com/shared/static/9ii2xb6z7869khz9xxrwcx1393a05610.zip
+https://nihcc.box.com/shared/static/2c7y53eees3a3vdls5preayjaf0mc3bn.zip"
+
+files=($files)
+num_files=${#files[@]}
+
+for (( i=0; i<${num_files}; i++ ));
+do
+    idx=$(printf "%02d" "$(expr $i + 1)")
+    wget ${files[$i]} -O Images_png_$idx.zip
+    unzip Images_png_$idx.zip
+done
+echo "DeepLesion dataset downloaded!"
